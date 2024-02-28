9fb84b5b179b69858d30a00ae2a35a7a0b519da2
Marek Karczewski <marekkarczewski05@gmail.com>
2024-02-28 14:16:08 +0100

    initicial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+alert('cześć');
+
+console.log('cześć');
+
+const age = '16'
+const name = 'Marek'
+
+console.log(name);
+console.log(age);
+
+let isHappy = false;
+isHappy = true;
+isHappy = 'tak';
+console.log(isHappy);
+
+console.log(`nazywam się ${name} i mam ${age} lat`);
+
